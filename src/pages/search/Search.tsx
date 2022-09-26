a60b59a3f1ce5be3e7a92ba6373bfb0a826ff1ce
poylib <poy_lib@naver.com>
2022-09-26 10:24:34 +0900

    Fix: API 명세에 맞게 수정

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -3,18 +3,33 @@ import { useEffect, useRef, useState, useContext } from 'react';
 import styled from 'styled-components';
 import { StyledHeader, StyledDiv } from '../notice/Notice';
 import { StyledList } from '../product/Product';
-
+import { BsChevronDown } from 'react-icons/bs';
+import { BiSearch } from 'react-icons/bi';
 interface ProductInfo {
   id: number;
-  beverageName: string;
-  imageURL: string;
+  beverage_name: string;
+  beverage_image: string;
   price: string;
+  description: string;
 }
 
 const Search = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [value, setValue] = useState('');
   const [product, setProduct] = useState<ProductInfo[]>([]);
+  const [option, setOption] = useState('이름');
+  const [optionOpen, setOptionOpen] = useState(false);
+
+  useEffect(() => {
+    const optionOpenHandler = ({ target }: MouseEvent) => {
+      if (target instanceof Element) {
+        target.closest('ul.option') ? setOptionOpen(o => !o) : setOptionOpen(false);
+      }
+    };
+
+    window.addEventListener('click', optionOpenHandler);
+    return () => window.removeEventListener('click', optionOpenHandler);
+  }, []);
 
   const searchHandler: React.FormEventHandler<HTMLFormElement> = e => {
     e.preventDefault();
@@ -38,10 +53,18 @@ const Search = () => {
         <h1>메뉴검색</h1>
         <p>일차의 다양한 메뉴를 검색해보세요.</p>
       </StyledHeader>
-      <StyledDiv>
+      <StyledDiv optionOpen={optionOpen}>
         <div className='container'>
+          <ul className='option'>
+            <li>
+              {option} <BsChevronDown />
+            </li>
+            <li onClick={() => setOption('이름')}>이름</li>
+            <li onClick={() => setOption('내용')}>내용</li>
+          </ul>
           <form onSubmit={searchHandler}>
-            <input type='text' ref={inputRef} placeholder='검색' />
+            <input type='text' ref={inputRef} placeholder='검색어를 입력해주세요.' />
+            <BiSearch />
           </form>
         </div>
       </StyledDiv>
@@ -49,10 +72,10 @@ const Search = () => {
         {product.map(info => (
           <li key={info.id}>
             <div className='imgContainer'>
-              <img src={info.imageURL} alt='productImage' />
+              <img src={info['beverage_image']} alt='productImage' />
             </div>
             <div className='container'>
-              <h3>{info.beverageName}</h3>
+              <h3>{info['beverage_name']}</h3>
               <h4>{Number(info.price).toLocaleString()}원</h4>
             </div>
           </li>

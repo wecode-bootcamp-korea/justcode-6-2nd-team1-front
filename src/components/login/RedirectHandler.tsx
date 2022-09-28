2d7fd0908c4f91b266ad76a9ed105564bf1d7ffc
poylib <poy_lib@naver.com>
2022-09-28 23:41:54 +0900

    Refactor/login (#29)
    * Refactor: 코드 수정중
    
    * fix: 버튼 수정
    
    * Refactor: 파일명 변경
    
    * Add: 카카오 소셜로그인 통신
    
    * Add: 카카오 통신 확인
    
    * Fix: 리뷰 반영 및 .env 추가
    
    * Fix: .env 삭제
    
    * Fix: 깃 충돌 해결

--- src/components/login/RedirectHandler.tsx
+++ src/components/login/RedirectHandler.tsx
@@ -3,16 +3,14 @@ import { useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import useStore from '../../context/store';
 import { useNavigate } from 'react-router-dom';
-import { KakaoReq, KakaoRes, KakaoPostReq } from '../../interface';
+import { KakaoReq, KakaoRes, KakaoPostRes } from '../../interface';
 
 const RedirectHandler = () => {
   const navigate = useNavigate();
   const { login, isLogin } = useStore();
   let codeURL = new URL(window.location.href).searchParams.get('code');
-  const RESTAPIKey = '95b40d30bfea9add60abe22405519b58';
-
   const getToken = async () => {
-    const { data: kakaoData } = await axios.post<KakaoPostReq>(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${RESTAPIKey}&redirect_uri=http://localhost:3000/kakaologin&code=${codeURL}`, {
+    const { data: kakaoData } = await axios.post<KakaoPostRes>(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${import.meta.env.VITE_API_KEY}&redirect_uri=http://localhost:3000/kakaologin&code=${codeURL}`, null, {
       headers: {
         'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
       },

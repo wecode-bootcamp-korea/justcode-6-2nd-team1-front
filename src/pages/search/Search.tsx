981f167bb080413b67f47db87f00142151d7d471
poylib <poy_lib@naver.com>
2022-09-30 00:03:20 +0900

    Fix: 검색결과 없을 때 문구 출력

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -7,27 +7,37 @@ import { BiSearch } from 'react-icons/bi';
 import { SearchInfo, ProductInfo } from '../../interface';
 import { useNavigate } from 'react-router-dom';
 import CategotySkeleton from '../product/CategorySkeleton';
+import theme from '../../theme';
 
 const Search = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
   const [value, setValue] = useState('');
   const [product, setProduct] = useState<ProductInfo[]>([]);
-  const timeId = useRef<NodeJS.Timeout>();
   const [loading, setLoading] = useState(true);
+  const [searchEmpty, setSearchEmpty] = useState(false);
+  const timeId = useRef<NodeJS.Timeout>();
 
   useEffect(() => {
     setLoading(true);
 
     (async () => {
-      const { data } = await axios.get<SearchInfo[]>(`http://localhost:8000/beverages/search?keyword=${value}`);
+      // const { data } = await axios.get<SearchInfo[]>(`http://localhost:8000/beverages/search?keyword=${value}`);
+      const { data } = await axios.get<SearchInfo[]>(`/data/search/search.json`);
       if (value) {
         const list = data.filter(search => {
           return search.beverage_name.includes(value);
         });
-        setProduct(list);
+        if (list.length) {
+          setProduct(list);
+          setSearchEmpty(false);
+        } else {
+          setSearchEmpty(true);
+          setProduct([]);
+        }
       } else {
         setProduct(data);
+        setSearchEmpty(false);
       }
       setLoading(false);
     })();
@@ -35,12 +45,10 @@ const Search = () => {
 
   const changeHandler: React.ChangeEventHandler<HTMLInputElement> = ({ target }) => {
     clearTimeout(timeId.current);
-
     timeId.current = setTimeout(() => {
       setValue(target.value);
     }, 500);
   };
-
   return (
     <StyledSearch>
       <StyledHeader>
@@ -59,6 +67,13 @@ const Search = () => {
         <CategotySkeleton />
       ) : (
         <StyledList>
+          {searchEmpty && (
+            <SearchResult>
+              <h4>검색 결과가 없습니다 !</h4>
+              <br />
+              <h3>다른 이름으로 검색해주세요</h3>
+            </SearchResult>
+          )}
           {product.map(info => (
             <li key={info.id} onClick={() => navigate(`/product/detail/${info.id}`)}>
               <div className='imgContainer'>
@@ -79,3 +94,19 @@ const Search = () => {
 export default Search;
 
 const StyledSearch = styled.div``;
+
+const SearchResult = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px 0;
+  h3 {
+    margin-bottom: 10px;
+    font-size: 5vw;
+  }
+
+  h4 {
+    font-size: 6vw;
+    color: ${theme.red};
+  }
+`;
